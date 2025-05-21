b0583be38fcf67c524ed85f925a46110255e71a2
fazzy01 <famaths01@gmail.com>
2025-05-21 16:57:33 +0100

    after working on knexfile issues on database migration

--- dist/middlewares/not-found.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = notFoundHandler;
-function notFoundHandler(req, res, next) {
-    res.status(404).json({
-        message: 'Not Found',
-    });
-}
